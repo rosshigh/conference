c3faad06f62462b9db7c70bb764203485aa6c9b1
Ross Hightower <hightowe@uwm.edu>
2019-04-05 11:48:50 -0500

    stuff

--- client/src/modules/home/logistics.js
+++ client/src/modules/home/logistics.js
@@ -1,3 +1,20 @@
 export class Logistics {
-    
+    constructor(){
+        this.showImage = [];
+        this.showImage.push("http://localhost/img/parallax1.jpg");
+        this.showImage.push("http://localhost/img/uwm.jpg");
+        this.showThisImage = this.showImage[0];
+        this.index = 1;
+    }
+
+    activate(){
+        setInterval(() => {
+            this.showThisImage = this.showImage[this.index];
+            if(this.index == this.showImage.length - 1){
+                this.index = 0;
+            } else {
+                this.index++;
+            }
+        },5000);
+    }
 }
