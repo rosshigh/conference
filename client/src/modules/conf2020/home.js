da3bad760527f3e28f5b68c7dcbdbb74de30326a
Ross Hightower <hightowe@uwm.edu>
2020-03-02 12:26:54 -0600

    stuff

--- client/src/modules/conf2020/home.js
+++ client/src/modules/conf2020/home.js
@@ -7,77 +7,84 @@ export class Conf2020Home {
         moduleId: './landing',
         name: 'Landing',
         settings: { auth: false, roles: [] },
-        title: 'SAP Next-Gen Chapter Conference 2019'
+        title: 'SAP Next-Gen Chapter Conference 2020'
       },
       {
         route: 'home',
         moduleId: './home',
         name: 'Home',
         settings: { auth: false, roles: [] },
-        title: 'SAP Next-Gen Chapter Conference 2019'
+        title: 'SAP Next-Gen Chapter Conference 2020'
       },
       {
         route: 'sponsorships',
         moduleId: './sponsorships',
         name: 'Sponsorships',
         settings: { auth: false, roles: [] },
-        title: 'SAP Next-Gen Chapter Conference 2019'
+        title: 'SAP Next-Gen Chapter Conference 2020'
       },
       {
         route: 'highlights',
         moduleId: './highlights',
         name: 'Highlights',
         settings: { auth: false, roles: [] },
-        title: 'SAP Next-Gen Chapter Conference 2019'
+        title: 'SAP Next-Gen Chapter Conference 2020'
       },
       {
         route: 'files',
         moduleId: './files',
         name: 'Files',
         settings: { auth: false, roles: [] },
-        title: 'SAP Next-Gen Chapter Conference 2019'
+        title: 'SAP Next-Gen Chapter Conference 2020'
       },
       {
         route: 'uploadFiles',
         moduleId: './uploadFiles',
         name: 'UploadFiles',
         settings: { auth: false, roles: [] },
-        title: 'SAP Next-Gen Chapter Conference 2019'
+        title: 'SAP Next-Gen Chapter Conference 2020'
       },
       {
         route: 'register',
         moduleId: './register',
         name: 'Register',
         settings: { auth: false, roles: [] },
-        title: 'SAP Next-Gen Chapter Conference 2019'
+        title: 'SAP Next-Gen Chapter Conference 2020'
+      },
+      {
+        route: 'social',
+        moduleId: './social',
+        name: 'Social',
+        settings: { auth: false, roles: [] },
+        title: 'SAP Next-Gen Chapter Conference 2020'
       },
       {
         route: 'logistics',
         moduleId: './logistics',
         name: 'Logistics',
         settings: { auth: false, roles: [] },
-        title: 'SAP Next-Gen Chapter Conference 2019'
+        title: 'SAP Next-Gen Chapter Conference 2020'
       },
       {
         route: 'agenda',
         moduleId: './agenda',
         name: 'Agenda',
         settings: { auth: false, roles: [] },
-        title: 'SAP Next-Gen Chapter Conference 2019'
+        title: 'SAP Next-Gen Chapter Conference 2020'
       },
       {
         route: 'submit',
         moduleId: './submit',
         name: 'Submission',
         settings: { auth: false, roles: [] },
-        title: 'SAP Next-Gen Chapter Conference 2019'
+        title: 'SAP Next-Gen Chapter Conference 2020'
       },
       {
         route: 'contact',
         moduleId: './contact',
         name: 'Contact',
         settings: { auth: false, roles: [] },
-        title: 'SAP Next-Gen Chapter Conference 2019'
+        title: 'SAP Next-Gen Chapter Conference 2020'
       }
     ])
   }
