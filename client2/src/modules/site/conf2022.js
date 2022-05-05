e8eb2107756757dc9aaddc9dc261de96acec7658
rosshigh <hightowe@uwm.edu>
2022-05-05 08:36:07 -0500

    paypal

--- client2/src/modules/site/conf2022.js
+++ client2/src/modules/site/conf2022.js
@@ -1,4 +1,50 @@
 
 export class Conf2022 {
     regularBanner = true;
+
+    activate() {
+        this.paypalURL = "https://www.paypal.com/sdk/js?client-id=AdG7HOB9ups2a4OOPuJuZKGadkv4qlFIXkkG4trDM_HKI3rl---nO0FEEyNPLHD-p-o8cWnNOdExGvfA&enable-funding=venmo&currency=USD&disable-funding=credit,venmo";
+    }
+
+    attached() {
+        setTimeout(() => { this.initPayPalButton(); }, 500);
+    }
+
+    initPayPalButton() {
+        paypal.Buttons({
+            style: {
+                shape: 'pill',
+                color: 'blue',
+                layout: 'vertical',
+                label: 'pay',
+
+            },
+
+            createOrder: function (data, actions) {
+                return actions.order.create({
+                    purchase_units: [{ "description": "Early Bird Conference Fee", "amount": { "currency_code": "USD", "value": 100 } }]
+                });
+            },
+
+            onApprove: function (data, actions) {
+                return actions.order.capture().then(function (orderData) {
+
+                    // Full available details
+                    console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+
+                    // Show a success message within this page, e.g.
+                    const element = document.getElementById('paypal-button-container');
+                    element.innerHTML = '';
+                    element.innerHTML = '<h3>Thank you for your payment!</h3>';
+
+                    // Or go to another URL:  actions.redirect('thank_you.html');
+
+                });
+            },
+
+            onError: function (err) {
+                console.log(err);
+            }
+        }).render('#paypal-button-container');
+    }
 }
