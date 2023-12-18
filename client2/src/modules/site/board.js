9e05b4b57b76919e1b0d386f02a2d44d926e5580
rosshigh <hightowe@uwm.edu>
2023-12-18 12:47:28 -0600

    updates

--- client2/src/modules/site/board.js
+++ client2/src/modules/site/board.js
@@ -29,7 +29,7 @@ export class Board {
 
     attached() {
         this.initGaia();
-        this.getBoard();
+        // this.getBoard();
     }
 
     searchString(data) {
@@ -260,21 +260,6 @@ export class Board {
         }
 
     };
-
-    // isElementInViewport(elem) {
-    //     var $elem = $(elem);
-
-    //     // Get the scroll position of the page.
-    //     var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
-    //     var viewportTop = $(scrollElem).scrollTop();
-    //     var viewportBottom = viewportTop + $(window).height();
-
-    //     // Get the position of the element on the page.
-    //     var elemTop = Math.round($elem.offset().top);
-    //     var elemBottom = elemTop + $elem.height();
-
-    //     return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
-    // }
 
 
 
